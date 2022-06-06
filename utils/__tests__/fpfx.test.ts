bd42bcf5297ace1176bdc3371135e7fad506518d
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-06-06 11:30:58 +0900

    feat: re tdd

--- utils/__tests__/fpfx.test.ts
+++ utils/__tests__/fpfx.test.ts
@@ -1,143 +1,83 @@
-import { map, filter, reduce } from "../fpfx";
+import { log } from "console";
+import { map, reduce, filter } from "../fpfx";
 
-interface TargetObjType {
-  id: string;
+interface InputObj {
   name: string;
-  type: "animal" | "fruit";
-  count: number;
+  age: number;
 }
 
-interface ResultObjType extends TargetObjType {
-  alive?: boolean;
+interface OutputObj extends InputObj {
+  blood?: "A" | "B" | "O" | "AB";
 }
 
-describe("map method", () => {
-  it("number type map", () => {
+describe("test map", () => {
+  it("basically type", () => {
     const target = [1, 2, 3, 4, 5];
     const add = (num: number) => num + 1;
 
     expect(map(add, target)).toEqual([2, 3, 4, 5, 6]);
   });
 
-  it("string type map", () => {
-    const target = "helloworld".split("");
-    const addStr = (str: string) => str + "!";
-
-    expect(map(addStr, target)).toEqual([
-      "h!",
-      "e!",
-      "l!",
-      "l!",
-      "o!",
-      "w!",
-      "o!",
-      "r!",
-      "l!",
-      "d!",
-    ]);
-  });
-
-  it("multi type map", () => {
-    const target = [1, 2, 3, 4, 5];
-    const convertNumberToString = (num: number) => `${num} is number!`;
-
-    expect(map(convertNumberToString, target)).toEqual([
-      "1 is number!",
-      "2 is number!",
-      "3 is number!",
-      "4 is number!",
-      "5 is number!",
-    ]);
-  });
-
-  it("object type map", () => {
-    const target: TargetObjType[] = [
-      { id: "1", name: "apple", type: "fruit", count: 4 },
-      { id: "2", name: "banana", type: "fruit", count: 5 },
-      { id: "3", name: "lion", type: "animal", count: 6 },
-      { id: "4", name: "melon", type: "fruit", count: 2 },
-      { id: "5", name: "tiger", type: "animal", count: 3 },
+  it("object type", () => {
+    const target: InputObj[] = [
+      { name: "a", age: 9 },
+      { name: "b", age: 12 },
+      { name: "c", age: 11 },
+      { name: "d", age: 10 },
     ];
-    const objFunc = (obj: TargetObjType): ResultObjType => {
-      const params: ResultObjType = { ...obj };
-      if (params.type === "animal") {
-        params.alive = true;
-      }
-      return params;
-    };
+    const addBlood = (item: InputObj): OutputObj =>
+      item.age % 2 === 0 ? item : { ...item, blood: "AB" };
 
-    expect(map(objFunc, target)).toEqual([
-      { id: "1", name: "apple", type: "fruit", count: 4 },
-      { id: "2", name: "banana", type: "fruit", count: 5 },
-      { id: "3", name: "lion", type: "animal", count: 6, alive: true },
-      { id: "4", name: "melon", type: "fruit", count: 2 },
-      { id: "5", name: "tiger", type: "animal", count: 3, alive: true },
+    expect(map(addBlood, target)).toEqual([
+      { name: "a", age: 9, blood: "AB" },
+      { name: "b", age: 12 },
+      { name: "c", age: 11, blood: "AB" },
+      { name: "d", age: 10 },
     ]);
   });
 });
 
-describe("filter method", () => {
-  it("number type filter", () => {
+describe("test filter", () => {
+  it("basically type", () => {
     const target = [1, 2, 3, 4, 5];
-    const condition = (num: number) => num % 3 === 1;
+    const odd = (item: number) => item % 2 === 0;
 
-    expect(filter(condition, target)).toEqual([1, 4]);
+    expect(filter(odd, target)).toEqual([2, 4]);
   });
 
-  it("string type filter", () => {
-    const target = "hello world".split("");
-    const isExist = (str: string) => str !== " ";
-
-    expect(filter(isExist, target)).toEqual([
-      "h",
-      "e",
-      "l",
-      "l",
-      "o",
-      "w",
-      "o",
-      "r",
-      "l",
-      "d",
-    ]);
-  });
-
-  it("object type filter", () => {
-    const target: TargetObjType[] = [
-      { id: "1", name: "apple", type: "fruit", count: 4 },
-      { id: "2", name: "banana", type: "fruit", count: 5 },
-      { id: "3", name: "lion", type: "animal", count: 6 },
-      { id: "4", name: "melon", type: "fruit", count: 2 },
-      { id: "5", name: "tiger", type: "animal", count: 3 },
+  it("object type", () => {
+    const target: InputObj[] = [
+      { name: "a", age: 9 },
+      { name: "b", age: 12 },
+      { name: "c", age: 11 },
+      { name: "d", age: 10 },
     ];
-    const objFunc = (obj: TargetObjType): boolean => obj.type === "animal";
+    const oddPeople = (item: InputObj) => item.age % 2 === 0;
 
-    expect(filter(objFunc, target)).toEqual([
-      { id: "3", name: "lion", type: "animal", count: 6 },
-      { id: "5", name: "tiger", type: "animal", count: 3 },
+    expect(filter(oddPeople, target)).toEqual([
+      { name: "b", age: 12 },
+      { name: "d", age: 10 },
     ]);
   });
 });
 
-describe("reduce method", () => {
-  it("typically reduce", () => {
-    const num = [1, 2, 3, 4, 5];
-    const add = (a: number, b: number): number => a + b;
+describe("test reduce", () => {
+  it("basically type", () => {
+    const target = [1, 2, 3, 4, 5];
+    const add = (acc: number, cur: number) => (acc += cur);
 
-    expect(reduce(add, 0, num)).toBe(15);
+    expect(reduce(add, 0, target)).toBe(15);
   });
 
-  it("accumulated string reduce", () => {
-    const strs = ["h", "e", "l", "l", "o"];
-    const accStr = (acc: string, str: string) => (acc += str);
+  it("object type", () => {
+    const target: InputObj[] = [
+      { name: "a", age: 9 },
+      { name: "b", age: 12 },
+      { name: "c", age: 11 },
+      { name: "d", age: 10 },
+    ];
+    const totalAge = (acc: number, item: InputObj): number => (acc += item.age);
 
-    expect(reduce(accStr, "", strs)).toBe("hello");
-  });
-
-  it("no initial value in reduce function", () => {
-    const num = [1, 2, 3, 4, 5];
-    const add = (a: number, b: number): number => a + b;
-
-    expect(reduce(add, num)).toBe(16);
+    expect(reduce(totalAge, 0, target)).toBe(42);
   });
 });
--- utils/fpfx.ts
+++ utils/fpfx.ts
@@ -4,26 +4,19 @@ export const map = <I, O>(func: (item: I) => O, iter: I[]): O[] => {
   return result;
 };
 
-export const filter = <I>(func: (item: I) => boolean, iter: I[]): I[] => {
+export const filter = <I, O>(func: (item: I) => boolean, iter: I[]): I[] => {
   const result = [];
-  for (const item of iter) if (func(item)) result.push(item);
+  for (const item of iter) {
+    if (func(item)) result.push(item);
+  }
   return result;
 };
 
 export const reduce = <I, O>(
-  func: (acc: O, cur: I) => O,
-  acc: O | I[],
-  iter?: I[]
-): O | I[] => {
-  let result = acc;
-  if (!iter && Array.isArray(acc)) {
-    iter = acc;
-    result = acc[Symbol.iterator]().next().value as O;
-    for (const item of iter) result = func(result, item);
-    return result;
-  } else if (iter && !Array.isArray(acc)) {
-    for (const item of iter) acc = func(acc, item);
-    return acc;
-  }
-  return result;
+  func: (acc: O, item: I) => O,
+  acc: O,
+  iter: I[]
+): O => {
+  for (const item of iter) acc = func(acc, item);
+  return acc;
 };
